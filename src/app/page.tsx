0a9149a6ceb677e3502229b2bd03ecc4e3ce859b
Arghya Chakrabarty <arghya2801@gmail.com>
2025-07-22 07:10:16 +0000

    Fixed unescaped entities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,11 @@ export default function Home() {
       <div className="text-lg font-semibold mb-6">Accountability App v3</div>
       <div className="flex flex-col md:flex-row gap-0 mb-6 width-full">
         <div className="flex-1 min-w-[50%]">
-          <div className="font-bold mb-2">{USER_A.name}'s goals</div>
+          <div className="font-bold mb-2">{`${USER_A.name}'s goals`}</div>
           <GoalsTable userId={USER_A.id} userName={USER_A.name} />
         </div>
         <div className="flex-1 min-w-[50%]">
-          <div className="font-bold mb-2">{USER_B.name}'s goals</div>
+          <div className="font-bold mb-2">{`${USER_B.name}'s goals`}</div>
           <GoalsTable userId={USER_B.id} userName={USER_B.name} />
         </div>
       </div>
