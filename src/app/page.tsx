108e931f2dc305b6bf7a53d0d1ca24e299d61368
Arghya Chakrabarty <arghya2801@gmail.com>
2025-07-22 08:14:35 +0000

    Added 2nd user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import AddGoalForm from "@/components/AddGoalForm";
 import GoalsTable from "@/components/GoalsTable";
 
 const USER_A = { id: 'a8f426c8-29e8-4855-9d9f-e9ef398fc26a', name: 'Arghya' }
-const USER_B = { id: 'UUID_FOR_USER_B', name: 'Bob' }
+const USER_B = { id: '5dcf715a-e8fb-45c2-b608-d691e7bd445b', name: 'Saksham' }
 
 export default function Home() {
   return (
@@ -11,11 +11,11 @@ export default function Home() {
       <div className="flex flex-col md:flex-row gap-0 mb-6 width-full">
         <div className="flex-1 min-w-[50%]">
           <div className="font-bold mb-2">{`${USER_A.name}'s goals`}</div>
-          <GoalsTable userId={USER_A.id} userName={USER_A.name} />
+          <GoalsTable userId={USER_A.id}/>
         </div>
         <div className="flex-1 min-w-[50%]">
           <div className="font-bold mb-2">{`${USER_B.name}'s goals`}</div>
-          <GoalsTable userId={USER_B.id} userName={USER_B.name} />
+          <GoalsTable userId={USER_B.id}/>
         </div>
       </div>
       <AddGoalForm />
